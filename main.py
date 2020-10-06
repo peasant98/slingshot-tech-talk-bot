9de0e1373fe738938e81d7cbeb3df20d62dc2ae6
Matthew Strong <mast4878@colorado.edu>
2020-10-05 21:59:18 -0600

    fix formatting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import random
+import discord
 import numpy as np
 
 from discord.ext import commands
@@ -14,31 +15,51 @@ if __name__ == '__main__':
     bot = commands.Bot(command_prefix='!')
 
     tech_talk_controller = TechTalkNewsController()
+    client = discord.Client()
 
     # decorator if needed for roles
     # @commands.has_role('admin')
 
-
-    @bot.command(name='tech_news', help='Get some cool tech news!')
-    async def find_tech_news(ctx):
-        print("here")
+    @client.event
+    async def on_ready():
         topic = np.random.choice(C.TECH_TOPICS)
         url, title, desc = tech_talk_controller.get_tech_news(topic)
         response = f"""
-                    **{title}**:\n{desc}\n{url}
+                    **Topic: {topic}**\n***{title}***:\n{desc}\n{url}
                     """
-        print(response)
-        await ctx.send(response)
+        channel_id = None
+        for guild in client.guilds:
+            if guild.name == 'SLINGSHOT':
+                break
+        for channel in guild.channels:
+            if channel.name == 'mentors':
+                channel_id = channel.id
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})'
+        )
+        await client.get_channel(channel_id).send(response)
 
-        response = random.choice(brooklyn_99_quotes)
-        await ctx.send(response)
+        exit()
 
+
+    # @bot.command(name='tech_news', help='Get some cool tech news!')
+    # async def find_tech_news(ctx):
+    #     print("here")
+    #     topic = np.random.choice(C.TECH_TOPICS)
+    #     url, title, desc = tech_talk_controller.get_tech_news(topic)
+    #     response = f"""
+    #                 **{title}**:\n{desc}\n{url}
+    #                 """
+    #     print(response)
+    #     await ctx.send(response)
+
+    #     response = random.choice(brooklyn_99_quotes)
+    #     await ctx.send(response)
 
     @bot.event
     async def on_command_error(ctx, error):
         if isinstance(error, commands.errors.CheckFailure):
             await ctx.send('You do not have the correct role for this command.')
 
-
-
-    bot.run(TOKEN)
+    client.run(TOKEN)
